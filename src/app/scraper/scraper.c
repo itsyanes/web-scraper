83a74e5cce5c164e9e5db7b421cfc2a1f492742a
TheGhostSpirit <quentin.caritey@outlook.fr>
2019-11-03 23:31:27 +0100

    scraping functions - wip

--- src/app/scraper/scraper.c
+++ src/app/scraper/scraper.c
@@ -19,6 +19,50 @@ void ScraperChangeOptions(Scraper *scraper, string uri, u_int8_t maxDepth, strin
     scraper->outputDir = outputDir;
 }
 
-void scrap()
+void scrap(Scraper *scraper)
 {
+    String *basePath = newString();
+    String *currentFilePath = newString();
+    string domainName = getDomainName(scraper->uri);
+    basePath->proto->build(basePath, "%s/%s", scraper->outputDir, domainName);
+    createDirectory(basePath);
+    free(domainName);
+    currentFilePath->proto->build(currentFilePath, "%s/%s", basePath, "index.html");
+    fetch(scraper->uri, currentFilePath, writeFile);
+    for (u_int8_t i = 0; i < scraper->maxDepth; i++)
+    {
+    }
+    basePath->proto->destroy(basePath);
+    currentFilePath->proto->destroy(currentFilePath);
+}
+
+size_t writeFile(char *ptr, size_t size, size_t nmemb, void *userdata)
+{
+    size_t size = 0;
+    FILE *f = fopen((char *)userdata, "w");
+    if (f)
+    {
+        size = fputs(ptr, f);
+        fclose(f);
+    }
+    else
+    {
+        perror("[ERROR]");
+    }
+    return size;
+}
+
+void createDirectory(string path)
+{
+    int status = mkdir(path, 0777);
+    if (status)
+    {
+        perror("[ERROR]");
+    }
+}
+
+string getDomainName(string uri)
+{
+    String *wrapper = wrapString(uri);
+    return uri;
 }
