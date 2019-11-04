7a7799f07d017a5d3b9ece0838589fcb541c1775
TheGhostSpirit <quentin.caritey@outlook.fr>
2019-11-04 21:58:53 +0100

    improved logging for curl requests

--- src/utils/http/http.c
+++ src/utils/http/http.c
@@ -1,6 +1,6 @@
 #include "http.h"
 
-void fetch(string uri, void *data, size_t (*callback)(char *ptr, size_t size, size_t nmemb, void *userdata))
+void fetch(string uri, char *resourceName, void *data, size_t (*callback)(char *ptr, size_t size, size_t nmemb, void *userdata))
 {
     CURL *curl = curl_easy_init();
 
@@ -15,7 +15,18 @@ void fetch(string uri, void *data, size_t (*callback)(char *ptr, size_t size, si
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, data);
 
+        string message = stringFromFormat("Downloading %s...", resourceName);
+        logger.info(message);
+        free(message);
+
         CURLcode res = curl_easy_perform(curl);
+
+        if (res == CURLE_OK)
+        {
+            message = stringFromFormat("%s was successfully downloaded!", resourceName);
+            logger.info(message);
+            free(message);
+        }
 
         if (res != CURLE_OK)
         {
